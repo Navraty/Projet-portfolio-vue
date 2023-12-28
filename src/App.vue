9b40633b018640e3be23342c58e3250395960a23
Navraty <tsamezojanne@gmail.com>
2023-12-28 10:46:13 +0100

    data-form

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {RouterView } from 'vue-router'
+import Accueil from './views/Accueil.vue'
+</script>
+
+<template>
+  <!-- <Accueil/> -->
+         <!-- <RouterLink to="/about">About</RouterLink>  -->
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
